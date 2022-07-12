5fcd6eefcd24a537e2c29cf7c84a208a649a69a2
barath46 <rajbarath999@gmail.com>
2022-07-12 15:55:05 +0530

    Slider component

---
+++ modules/slider.tsx
@@ -0,0 +1,29 @@
+import { Slider, Switch, Typography, Divider } from 'antd';
+import React from 'react';
+
+const { Title } = Typography;
+
+const style: React.CSSProperties = {
+  display: 'inline-block',
+  height: 300,
+  marginLeft: 70,
+};
+
+const App = () => {
+  return (
+    <div id="slider-component">
+      <Title level={2}>Slider component:</Title>
+      <Title level={5}>Basic Slider:</Title>
+      <Slider defaultValue={30} />
+      <Slider range defaultValue={[20, 50]} disabled />
+      <br />
+      <br />
+      <Title level={5}>Vertical slider:</Title>
+      <div style={style}>
+        <Slider vertical defaultValue={30} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
